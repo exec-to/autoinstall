4222df540ec5e48ab688687f8455e5dc76b0b565
Roman Sokolov <sokolov-at@yandex.ru>
2020-04-15 13:06:42 +0300

    dev stage 15/04@01

--- app/api/servers.py
+++ app/api/servers.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from flask_restx import Namespace, Resource, errors, fields
+from flask_restx import Namespace, Resource, errors, fields, abort
 from app import config
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_filters import apply_filters
@@ -8,19 +8,12 @@ import os
 from app import core as CoreLib
 from app.core.grub import Grub
 
-api = Namespace('servers', description='Server related operations')
+api = Namespace('servers', description='Раздел: операции с серверами')
 core = CoreLib.Core()
 
-# TODO: добавить MAC-адреса к выводу
-ServerModel = api.model('Model', {
-    'id': fields.String,
-    'adman_id': fields.String,
-    'uri': fields.Url('api.server_ep', absolute = True),
-    'message': fields.String
-})
 
 MacAddressModel = api.model('MacAddressModel', {
-    'id': fields.String,
+    # 'id': fields.String,
     'mac_addr': fields.String
 })
 
@@ -28,6 +21,13 @@ MacListModel = api.model('MacListModel', {
     'mac_list': fields.List(fields.Nested(MacAddressModel))
 })
 
+ServerModel = api.model('Model', {
+    'id': fields.String,
+    'adman_id': fields.String,
+    'uri': fields.Url('api.server_ep', absolute = True),
+    'message': fields.String,
+    'mac_list': fields.List(fields.Nested(MacAddressModel))
+})
 
 @api.route('/<int:adman_id>', endpoint='server_ep')
 class Server(Resource):
@@ -35,30 +35,33 @@ class Server(Resource):
     @api.marshal_with(ServerModel, skip_none=True)
     def get(self, adman_id):  # Create GET endpoint
         """
-        Получить информацию по серверу
+        Получить информацию по указанному серверу
         """
+        session = None
+
         headers = request.headers
         auth = headers.get("X-Api-Key")
         if auth != config.auth['adman']:
-            api.logger.debug('Unauthorized, 401')
-            return {"message": "Error: Unauthorized"}, 401
+            abort(401)
         try:
             session = sessionmaker(bind=core.engine)()
         except Exception as e:
-            msg = 'Can\'t initialize session. Error: {}'.format(str(e))
+            msg = 'Не удаётся инициализировать соединение с БД: {}'.format(str(e))
             api.logger.error(msg)
-            return {"message": msg}, 500
+            abort(500, message=msg)
 
+        server = None
         try:
             server = session.query(CoreLib.Server).filter_by(adman_id=adman_id).first()
             if server is None:
-                api.logger.debug('None server with id 200:1')
                 raise Exception('NotFound', 'Не найден сервер c ID: {}'.format(adman_id))
 
+            server.mac_list = session.query(CoreLib.MacTable).filter_by(server_id=server.id).all()
+
         except Exception as e:
-            msg = 'Can\'t get server. Error: {}'.format(str(e))
+            msg = 'Не удаётся получить сервер. Ошибка: {}'.format(str(e))
             api.logger.error(msg)
-            return {"id": None, "adman_id": adman_id, "uri": "", "message": msg}, 500
+            abort(400, message=msg)
 
         return server
 
@@ -69,99 +72,102 @@ class ServerList(Resource):
     @api.marshal_list_with(ServerModel)
     def get(self):  # Create GET endpoint
         """
-        returns a list of servers
+        Получить список настроенных серверов
         """
+        session = None
+
         headers = request.headers
         auth = headers.get("X-Api-Key")
         if auth != config.auth['adman']:
             api.logger.debug('Unauthorized, 401')
-            return {"message": "Error: Unauthorized"}, 401
+            abort(401)
         try:
             session = sessionmaker(bind=core.engine)()
         except Exception as e:
-            msg = 'Can\'t initialize session. Error: {}'.format(str(e))
+            msg = 'Не удаётся инициализировать соединение с БД: {}'.format(str(e))
             api.logger.error(msg)
-            return {"message": msg}, 500
+            abort(500, message=msg)
 
+        servers = None
         try:
             servers = session.query(CoreLib.Server).all()
 
+            for server in servers:
+                server.mac_list = session.query(CoreLib.MacTable).filter_by(server_id=server.id).all()
+
         except Exception as e:
-            msg = 'Can\'t get servers. Error: {}'.format(str(e))
+            msg = 'Не удаётся получить список серверов. Ошибка: {}'.format(str(e))
             api.logger.error(msg)
-            return {"message": msg}, 500
+            abort(400, message=msg)
 
         return servers
 
 
-# @api.route('/configure/<int:adman_id>', endpoint='server_configure_ep')
-# class ServerConfigure(Resource):
-#     @api.doc(security='apikey')
-#     @api.expect(MacListModel)
-#     def put(self, adman_id, server_config):  # Create GET endpoint
-#         """
-#         Configure server
-#         """
-#         headers = request.headers
-#         api.logger.debug(headers)
-#         auth = headers.get("X-Api-Key")
-#         if auth != config.auth['adman']:
-#             api.logger.debug('Unauthorized, 401')
-#             return {"message": "Error: Unauthorized"}, 401
+@api.route('/configure/<int:adman_id>', endpoint='server_configure_ep')
+class ServerConfigure(Resource):
+    @api.doc(security='apikey')
+    @api.expect([MacAddressModel])
+    def put(self, adman_id):  # Create PUT endpoint
+        """
+        Обновить конфигурацию указанного сервера
+        """
+        headers = request.headers
+        api.logger.debug(headers)
+        auth = headers.get("X-Api-Key")
+        if auth != config.auth['adman']:
+            api.logger.debug('Unauthorized, 401')
+            abort(401)
 
-        # parser = reqparse.RequestParser(bundle_errors=True)
-        # parser.add_argument('mac_addr', action='append', help="List of server MAC-address")
-        # args = parser.parse_args()
+        # api.logger.debug(request.json)
+        session = None
+        try:
+            session = sessionmaker(bind=core.engine)()
+        except Exception as e:
+            msg = 'Не удаётся инициализировать соединение с БД: {}'.format(str(e))
+            api.logger.error(msg)
+            abort(500, message=msg)
 
-        # app.logger.debug("args: {}".format(server_config))
+        server = None
+        try:
+            server = session.query(CoreLib.Server).filter_by(adman_id=adman_id).first()
+            if server is None:
+                server = CoreLib.Server(adman_id=adman_id)
+                session.add(server)
+                session.commit()
 
-        # try:
-        #     session = sessionmaker(bind=core.engine)()
-        # except Exception as e:
-        #     msg = 'Can\'t initialize session. Error: {}'.format(str(e))
-        #     api.logger.error(msg)
-        #     return {"message": msg}, 500
-        #
-        # try:
-        #     server = session.query(CoreLib.Server).filter_by(adman_id=adman_id).first()
-        #     if server is None:
-        #         server = CoreLib.Server(adman_id=adman_id)
-        #         session.add(server)
-        #         session.commit()
-        #
-        #     # app.logger.debug("adman_id: {}".format(server.adman_id))
-        #
-        #     if hasattr(args, 'mac_addr') and args.mac_addr is not None:
-        #         for addr in args.mac_addr:
-        #             mac_exist = False
-        #             # query to check mac-address exist
-        #             filters = [
-        #                 {'field': 'mac_addr', 'op': 'ilike', 'value': addr}
-        #             ]
-        #             macs = session.query(CoreLib.MacTable)
-        #             macs = apply_filters(macs, filters)
-        #             for mac in macs:
-        #                 if mac.server_id == server.adman_id:
-        #                     mac_exist = True
-        #                 else:
-        #                     msg = 'MAC-address already used by adman server ID: {}'.format(mac.server_id)
-        #                     api.logger.error(msg)
-        #                     return {"message": msg}, 500
-        #
-        #             if mac_exist:
-        #                 continue
-        #
-        #             macaddr = CoreLib.MacTable(server_id=server.adman_id, mac_addr=addr)
-        #             api.logger.debug('Add MAC: {}'.format(addr))
-        #             session.add(macaddr)
-        #         session.commit()
-        #
-        # except Exception as e:
-        #     msg = 'Can\'t configure server. Error: {}'.format(str(e))
-        #     api.logger.error(msg)
-        #     return {"message": msg}, 500
+            # TODO: Изменить логику. Удалять старые MAC-адреса сервера.
+            # TODO: Также сразу создавать каталоги и символьные ссылки для сервера по мак-адресам.
+            for item in request.json:
+                # api.logger.debug("mac: {}".format(item['mac_addr']))
+                mac_exist = False
+                # query to check mac-address exist
+                filters = [
+                    {'field': 'mac_addr', 'op': 'ilike', 'value': item['mac_addr']}
+                ]
+                macs = session.query(CoreLib.MacTable)
+                macs = apply_filters(macs, filters)
+                for mac in macs:
+                    if mac.server_id == server.id:
+                        mac_exist = True
+                    else:
+                        msg = 'MAC-адрес используется сервером с ID: {}'.format(mac.server_id)
+                        api.logger.error(msg)
+                        return {"message": msg}, 500
 
-        # return {"server.id": server.id}
+                if mac_exist:
+                    continue
+
+                macaddr = CoreLib.MacTable(server_id=server.id, mac_addr=item['mac_addr'])
+                api.logger.debug('Add MAC: {}'.format(item['mac_addr']))
+                session.add(macaddr)
+            session.commit()
+
+        except Exception as e:
+            msg = 'Не удаётся настроить сервер. Ошибка: {}'.format(str(e))
+            api.logger.error(msg)
+            abort(400, message=msg)
+
+        return {"server.id": server.id}
 
 
 # # server_install_put_parser = reqparse.RequestParser(bundle_errors=True)
